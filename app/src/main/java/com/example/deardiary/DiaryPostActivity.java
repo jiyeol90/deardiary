351fa4378bb5748e3d838fcf2b74ca1f94d70c34
james <jiyeol9023@gmail.com>
2021-01-26 17:10:23 +0900

    서버 변경

---
+++ app/src/main/java/com/example/deardiary/DiaryPostActivity.java
@@ -0,0 +1,14 @@
+package com.example.deardiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiaryPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_post);
+    }
+}
